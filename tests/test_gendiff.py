2fa3a9950aa51bcc63fe05312399eb2d345baca6
[Buchik] <[yaBuchik@yandex.ru]>
2026-02-16 16:17:51 +0300

    created test for generate_diff function

---
+++ tests/test_gendiff.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from gendiff import generate_diff
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_generate_diff():
+    file1 = str(get_test_data_path("file1.json"))
+    file2 = str(get_test_data_path("file2.json"))
+    expected = read_file("result_plain.txt")
+    actual = generate_diff(file1, file2)
+    assert actual == expected
